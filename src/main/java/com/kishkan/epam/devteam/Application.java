06281727fff27dd513da487e952eabbf947333b1
Ivan <Kishkan91@gmail.com>
2019-07-31 20:33:49 +0400

    StaffCompetenceGrade is added

--- src/main/java/com/kishkan/epam/devteam/Application.java
+++ src/main/java/com/kishkan/epam/devteam/Application.java
@@ -1,7 +1,6 @@
 package com.kishkan.epam.devteam;
 
-import com.kishkan.epam.devteam.config.DevelopersMockProperties;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.kishkan.epam.devteam.config.StaffMockProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,14 +9,11 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
 
-	@Autowired
-	private DevelopersMockProperties developersMockProperties;
-
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
-		DevelopersMockProperties developersMockProperties = context.getBean(DevelopersMockProperties.class);
+		StaffMockProperties staffMockProperties = context.getBean(StaffMockProperties.class);
 
 		System.out.println("LOG: app start");
-		System.out.println(developersMockProperties.getDevelopers());
+		System.out.println(staffMockProperties);
 	}
 }
---
+++ src/main/java/com/kishkan/epam/devteam/support/enums/StaffCompetenceGrade.java
@@ -0,0 +1,3 @@
+package com.kishkan.epam.devteam.support.enums;
+
+public enum StaffCompetenceGrade {D1, D2, D3, D4, D5}
